691b10e3156c67869f6d72a66ed07b3d661830d1
Patrick Arminio <patrick.arminio@gmail.com>
2019-03-13 23:31:47 +0000

    Add favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,15 +46,36 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-favicon`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        logo: "./static/logo.png",
+
+        // WebApp Manifest Configuration
+        appName: "Strawberry GraphQL", // Inferred with your package.json
+        appDescription: "A Python GraphQL Library",
+        developerName: null,
+        developerURL: null,
+        dir: "auto",
+        lang: "en-GB",
+        background: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        orientation: "any",
+        start_url: "/?homescreen=1",
+        version: "1.0",
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
       },
     },
   ],
