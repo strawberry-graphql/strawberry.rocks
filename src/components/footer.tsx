b1bc2a331185c95928544dcd0b9dc02e192d3043
Patrick Arminio <patrick.arminio@gmail.com>
2019-11-02 20:17:50 +0000

    Fix footer mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -21,27 +21,43 @@ export const Footer: React.SFC = () => (
     as="footer"
     sx={{
       py: 5,
-      px: 3,
+      px: 4,
       backgroundColor: "black",
       color: "white",
     }}
   >
     <Grid
-      columns={[4]}
       sx={{
         mx: "auto",
         maxWidth: 1280,
+        gridTemplateColumns: ["repeat(4, 1fr)", "140px repeat(3, 1fr)"],
       }}
     >
-      <Logo width="100" />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: ["center", "flex-start"],
+          gridColumnStart: [1, 1],
+          gridColumnEnd: [5, 2],
+          mb: 3,
+        }}
+      >
+        <Logo width="100" />
+      </Box>
       <nav>
         <NavItem href="/docs/">Documentation</NavItem>
       </nav>
-      <nav></nav>
-      <nav>
+      <Box
+        as="nav"
+        sx={{
+          textAlign: "right",
+          gridColumnStart: [2, 3],
+          gridColumnEnd: [5, 5],
+        }}
+      >
         <NavItem href="https://twitter.com/patrick91">Twitter</NavItem>
         <NavItem href="https://twitter.com/patrick91">Github</NavItem>
-      </nav>
+      </Box>
     </Grid>
   </Box>
 );
