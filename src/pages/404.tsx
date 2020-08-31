8a74c2daef4a5ac122b6e7d2abb50fb4055d1ff7
Patrick Arminio <patrick.arminio@gmail.com>
2020-08-31 23:11:43 +0200

    Improve 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,25 @@
 import * as React from "react";
+import { Flex, Box } from "@theme-ui/components";
 
 import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <Flex
+      sx={{
+        width: "100%",
+        maxWidth: 1200,
+        mx: "auto",
+        flex: 1,
+      }}
+    >
+      <Box sx={{ px: 4, pb: 6 }}>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Box>
+    </Flex>
   </>
 );
 
