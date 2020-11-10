e12ca08ae945c49acb6da2cb237a8c6b0b3ea312
Jonathan Kim <jkimbo@gmail.com>
2020-11-10 17:38:45 +0000

    Add custom font and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import { ThemeProvider } from "theme-ui";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import theme from "../theme";
 import components from "~/components/theme-ui";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // @ts-ignore components prop is missing in the type definition
-    <ThemeProvider theme={theme} components={components}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Karla:,400,400i,700&amp;display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      {/* @ts-ignore components prop is missing in the type definition */}
+      <ThemeProvider theme={theme} components={components}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
