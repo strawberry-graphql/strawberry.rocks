44f22f3798db620480421eb389f4ad17523e31b2
Patrick Arminio <patrick.arminio@gmail.com>
2020-12-05 15:50:08 +0000

    Update seo component

--- components/seo.tsx
+++ components/seo.tsx
@@ -1,4 +1,3 @@
-import { graphql, StaticQuery } from "gatsby";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 
@@ -12,104 +11,77 @@ interface SEOProps {
 
 const socialImageUrl = "https://strawberry.rocks/social.png";
 
-const DETAILS_QUERY = graphql`
-  query DefaultSEOQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        author
-      }
-    }
-  }
-`;
-
-const SEO: React.SFC<SEOProps> = ({
-  description,
-  lang,
-  meta,
-  keywords,
+export const SEO = ({
+  description = `Strawberry is Python library to build GraphQL APIs, built on top of dataclasses`,
+  lang = "en",
+  meta = [],
+  keywords = [],
   title,
-}): JSX.Element => (
-  <StaticQuery
-    query={DETAILS_QUERY}
-    render={(data) => {
-      const metaDescription = description || data.site.siteMetadata.description;
-      return (
-        <Helmet
-          htmlAttributes={{
-            lang,
-          }}
-          title={title}
-          titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-          meta={[
-            {
-              content: metaDescription,
-              name: "description",
-            },
-            {
-              content: title,
-              property: "og:title",
-            },
-            {
-              content: metaDescription,
-              property: "og:description",
-            },
-            {
-              content: "website",
-              property: "og:type",
-            },
-            {
-              content: "summary_large_image",
-              name: "twitter:card",
-            },
-            {
-              content: data.site.siteMetadata.author,
-              name: "twitter:creator",
-            },
-            {
-              content: title,
-              name: "twitter:title",
-            },
-            {
-              content: metaDescription,
-              name: "twitter:description",
-            },
-            {
-              content: socialImageUrl,
-              name: "twitter:image",
-            },
-            {
-              content: socialImageUrl,
-              name: "og:image",
-            },
-          ]
-            .concat(
-              keywords.length > 0
-                ? {
-                    content: keywords.join(", "),
-                    name: "keywords",
-                  }
-                : []
-            )
-            .concat(meta)}
-        >
-          <script
-            async
-            defer
-            data-domain="strawberry.rocks"
-            src="https://stats.strawberry.rocks/js/index.js"
-          ></script>
-        </Helmet>
-      );
+}: SEOProps): JSX.Element => (
+  <Helmet
+    htmlAttributes={{
+      lang,
     }}
-  />
+    title={title}
+    titleTemplate={"%s | 🍓 Strawberry GraphQL"}
+    meta={[
+      {
+        content: description,
+        name: "description",
+      },
+      {
+        content: title,
+        property: "og:title",
+      },
+      {
+        content: description,
+        property: "og:description",
+      },
+      {
+        content: "website",
+        property: "og:type",
+      },
+      {
+        content: "summary_large_image",
+        name: "twitter:card",
+      },
+      {
+        // TODO: maybe in future we'll have a strawberry twitter account
+        content: "patrick91",
+        name: "twitter:creator",
+      },
+      {
+        content: title,
+        name: "twitter:title",
+      },
+      {
+        content: description,
+        name: "twitter:description",
+      },
+      {
+        content: socialImageUrl,
+        name: "twitter:image",
+      },
+      {
+        content: socialImageUrl,
+        name: "og:image",
+      },
+    ]
+      .concat(
+        keywords.length > 0
+          ? {
+              content: keywords.join(", "),
+              name: "keywords",
+            }
+          : []
+      )
+      .concat(meta)}
+  >
+    <script
+      async
+      defer
+      data-domain="strawberry.rocks"
+      src="https://stats.strawberry.rocks/js/index.js"
+    ></script>
+  </Helmet>
 );
-
-SEO.defaultProps = {
-  keywords: [],
-  lang: "en",
-  meta: [],
-};
-
-export default SEO;
