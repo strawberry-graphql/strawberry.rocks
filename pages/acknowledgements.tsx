3c1a62cfd83c0838ce9be98ac01df5f3fc5a7407
Patrick Arminio <patrick.arminio@gmail.com>
2021-01-16 23:54:19 +0100

    Update acknowledgements

--- pages/acknowledgements.tsx
+++ pages/acknowledgements.tsx
@@ -1,10 +1,9 @@
 /** @jsx jsx */
-import { Text, Box, Flex, Heading } from "@theme-ui/components";
+import { Text, Box, Flex, Heading, Paragraph } from "@theme-ui/components";
 import { jsx } from "theme-ui";
 
 import { Link } from "~/components/link";
 import { SEO } from "~/components/seo";
-import MDXDocument from "~/content/acknowledgements.mdx";
 import { githubQuery } from "~/github";
 
 const IGNORE_LIST = ["dependabot-preview[bot]", "dependabot-bot", "botberry"];
@@ -44,7 +43,7 @@ const AcknowledgementsPage = ({
 
       <Box sx={{ p: 4, pb: 6, maxWidth: 1280, mx: "auto" }}>
         <Heading sx={{ fontSize: [5, 6], mb: 3 }}>Acknowledgements</Heading>
-        <Text sx={{ mb: 4 }}>
+        <Text sx={{ mb: 4 }} as="p">
           We&apos;d like to thank all of our contributors:
         </Text>
 
@@ -64,7 +63,66 @@ const AcknowledgementsPage = ({
           ))}
         </Flex>
 
-        <MDXDocument />
+        <Box>
+          <Heading sx={{ my: 4 }}>
+            In addition we&apos;d like to thank also the following people:
+          </Heading>
+          <ul>
+            <li>
+              <Link href="https://twitter.com/druguinni">Orlando Festa</Link>{" "}
+              for the fantastic work on the Strawberry logo.
+            </li>
+
+            <li>
+              <Link href="https://twitter.com/BijlsmaLotte">Lotte Bijlsma</Link>{" "}
+              for the amazing design of this website.
+            </li>
+          </ul>
+
+          <Heading sx={{ mt: 4, mb: 2 }}>Icons:</Heading>
+          <Paragraph sx={{ mb: 3 }}>
+            We are using icons from{" "}
+            <Link href="https://thenounproject.com">The Noun Project</Link>.
+            Here&apos;s the list of icons we used on this website:
+          </Paragraph>
+
+          <ul>
+            <li>
+              Async by{" "}
+              <Link href="https://thenounproject.com/t.rostilov/">
+                Timofey Rostilov
+              </Link>
+            </li>
+            <li>
+              Server by{" "}
+              <Link href="https://thenounproject.com/clea.doltz/">
+                Clea Doltz
+              </Link>
+            </li>
+            <li>
+              Python by{" "}
+              <Link href="https://thenounproject.com/priyokumoro5/">
+                Zaenal Abidin
+              </Link>
+            </li>
+            <li>
+              Boxes by{" "}
+              <Link href="https://thenounproject.com/iconsbazaar89/">
+                Icons Bazaar
+              </Link>
+            </li>
+            <li>
+              Navigation by{" "}
+              <Link href="https://thenounproject.com/Flatart/">Flatart</Link>
+            </li>
+            <li>
+              Close by{" "}
+              <Link href="https://thenounproject.com/landan/">
+                Landan Lloyd
+              </Link>
+            </li>
+          </ul>
+        </Box>
       </Box>
     </>
   );
