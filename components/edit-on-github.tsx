cc2306f2540c168f2902afb8c96135cd70b99fc4
Patrick Arminio <patrick.arminio@gmail.com>
2021-03-23 23:48:42 +0000

    better edit on github button on dark mode

--- components/edit-on-github.tsx
+++ components/edit-on-github.tsx
@@ -6,7 +6,7 @@ export const EditOnGithub = ({ path }: EditOnGithubProps) => (
   <Link
     href={path}
     variant="box"
-    className="mt-12 border-2 border-blue-600 p-4 hover:bg-gray-50 text-blue-600 flex items-center justify-center"
+    className="mt-12 border-2 border-blue-600 p-4 hover:bg-gray-50 text-blue-600 flex items-center justify-center dark:border-blue-300 dark:text-blue-300 dark:hover:bg-blue-900"
   >
     Edit on Github
   </Link>
