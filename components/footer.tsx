64160aa5d9e04d63c659a3aaccb9eea10e28805f
Patrick Arminio <patrick.arminio@gmail.com>
2021-03-26 21:31:56 +0000

    Fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,17 +16,17 @@ const NavItem = ({ href, children, ...props }: NavItemProps) => (
 );
 
 export const Footer = () => (
-  <footer className="bg-black text-white py-12 px-8 dark:bg-gray-800">
-    <div className="grid mx-auto w-full max-w-7xl grid-cols-2">
+  <footer className="bg-black text-white py-12  dark:bg-gray-800">
+    <div className="grid mx-auto w-full max-w-7xl px-8 grid-cols-2 md:flex">
       <div className="flex justify-center md:justify-start row-start-2 md:row-start-1 col-start-1 col-end-3 md:col-end-2 mt-6 md:mt-0">
         <Logo width="100" />
       </div>
-      <nav>
+      <nav className="md:ml-10">
         <NavItem href="/docs/">Documentation</NavItem>
         <NavItem href="/acknowledgements/">Acknowledgements</NavItem>
         <NavItem href="/code-of-conduct/">Code of Conduct</NavItem>
       </nav>
-      <nav className="text-right col-start-2 md:col-start-3 col-end-3 md:col-end-5">
+      <nav className="text-right col-start-2 col-end-3 md:ml-auto">
         <NavItem target="_blank" href="https://twitter.com/patrick91">
           Twitter
         </NavItem>
