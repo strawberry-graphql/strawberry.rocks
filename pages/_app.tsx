421ea4ad1a393e2184b1ee36858bf7a28917ec5e
Patrick Arminio <patrick.arminio@gmail.com>
2022-10-27 00:08:35 -0700

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@docsearch/css/dist/style.css";
 import NProgress from "nprogress";
 
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 import Router from "next/router";
 
 import { Footer } from "~/components/footer";
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <NewsletterSection />
         <Footer />
+        <Analytics />
       </div>
     </>
   );
