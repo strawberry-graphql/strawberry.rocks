51af8e7527114dd1546befdca930f3c7257a9059
Patrick Arminio <patrick.arminio@gmail.com>
2023-03-02 00:05:21 -0600

    Remove unused code

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -2,74 +2,14 @@ import { DocsContent, PageTOC } from "@strawberry-graphql/styleguide";
 import remarkComment from "remark-comment";
 import remarkGfm from "remark-gfm";
 import shiki from "shiki";
-import type { Highlighter } from "shiki";
 
-import {
-  CompileMDXResult,
-  MDXRemoteProps,
-  compileMDX,
-} from "next-mdx-remote/rsc";
-import { serialize } from "next-mdx-remote/serialize";
+import { compileMDX } from "next-mdx-remote/rsc";
 
 import { components } from "~/components/mdx";
 import { fixImagePathsPlugin } from "~/helpers/image-paths";
 import { fetchDocPage, OWNER, REF, REPO } from "~/lib/api";
 import { RehypeHighlightCode } from "~/rehype-plugins/rehype-highlight-code";
 import { RehypeTOC, TocItem } from "~/rehype-plugins/rehype-toc";
-
-// let jsxRuntime;
-
-// if (process.env.NODE_ENV === "development") {
-//   jsxRuntime = require("react/jsx-dev-runtime");
-// } else {
-//   jsxRuntime = require("react/jsx-runtime");
-// }
-// async function compileMDX<TFrontmatter = Record<string, unknown>>({
-//   source,
-//   options,
-//   components = {},
-// }: MDXRemoteProps): Promise<CompileMDXResult<TFrontmatter>> {
-//   const { compiledSource, frontmatter, scope } = await serialize<
-//     Record<string, unknown>,
-//     TFrontmatter
-//   >(
-//     source,
-//     options,
-//     // Enable RSC importSource
-//     true
-//   );
-//   // if we're ready to render, we can assemble the component tree and let React do its thing
-//   // first we set up the scope which has to include the mdx custom
-//   // create element function as well as any components we're using
-//   const fullScope = Object.assign(
-//     {
-//       opts: jsxRuntime,
-//     },
-//     { frontmatter },
-//     scope
-//   );
-//   const keys = Object.keys(fullScope);
-//   const values = Object.values(fullScope);
-
-//   // now we eval the source code using a function constructor
-//   // in order for this to work we need to have React, the mdx createElement,
-//   // and all our components in scope for the function, which is the case here
-//   // we pass the names (via keys) in as the function's args, and execute the
-//   // function with the actual values.
-//   const hydrateFn = Reflect.construct(
-//     Function,
-//     keys.concat(`${compiledSource}`)
-//   );
-
-//   console.log("aaa", compiledSource);
-
-//   const Content: React.ElementType = hydrateFn.apply(hydrateFn, values).default;
-
-//   return {
-//     content: <Content components={components} />,
-//     frontmatter,
-//   };
-// }
 
 export default async function DocsPage({
   params,
