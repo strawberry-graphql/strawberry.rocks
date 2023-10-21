115728aa55dc76ee177fef9844c8e158e43365e6
Patrick Arminio <patrick.arminio@gmail.com>
2023-10-21 22:19:12 +0100

    bump

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   swcMinify: true,
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "tsx"],
