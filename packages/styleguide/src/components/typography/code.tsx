677ceb0302fe5ed5a5a53293a26bcc6fd475ff11
Patrick Arminio <patrick.arminio@gmail.com>
2023-10-24 12:08:45 +0100

    Initial idea on how the API reference home could look like

--- packages/styleguide/src/components/typography/code.tsx
+++ packages/styleguide/src/components/typography/code.tsx
@@ -1,11 +1,18 @@
 import clsx from "clsx";
 
-export const Code = ({ children }: { children: React.ReactNode }) => {
+export const Code = ({
+  children,
+  className,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
   return (
     <code
       className={clsx(
         "typography-code rounded-[4px] px-[6px] py-2 ",
-        "text-strawberry bg-inline-code border border-strawberry"
+        "text-strawberry bg-inline-code border border-strawberry",
+        className
       )}
     >
       {children}
