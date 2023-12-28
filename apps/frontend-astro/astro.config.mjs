a24de53e9424e6806a5db22cc95ae596ca33ddc7
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-29 00:04:19 +0100

    Mailchimp

--- apps/frontend-astro/astro.config.mjs
+++ apps/frontend-astro/astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    output: "hybrid",
     site: "https://strawberry.rocks",
     integrations: [sitemap()]
 });
