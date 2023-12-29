e569efe258688c8beb3fce3d087f575a38e1cbce
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-29 15:33:23 +0100

    Fix slashes

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'never',
   output: "hybrid",
   site: "https://strawberry.rocks",
   integrations: [sitemap()],
