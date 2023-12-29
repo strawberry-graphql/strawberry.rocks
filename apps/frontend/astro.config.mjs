dbd10fbe4357d4dce5ffd227ec4a67a7ce964ae7
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-29 15:02:46 +0100

    Vercel adapter

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "hybrid",
-    site: "https://strawberry.rocks",
-    integrations: [sitemap()]
+  output: "hybrid",
+  site: "https://strawberry.rocks",
+  integrations: [sitemap()],
+  adapter: vercel()
 });
