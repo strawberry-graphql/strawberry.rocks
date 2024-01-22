3c9522945aa4b72da5c3c83426f9eb17f81c6df4
Patrick Arminio <patrick.arminio@gmail.com>
2024-01-22 20:47:31 +0100

    Server output

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'never',
-  output: "hybrid",
+  output: "server",
   site: "https://strawberry.rocks",
   integrations: [sitemap(), astroMetaTags()],
   adapter: vercel(),
