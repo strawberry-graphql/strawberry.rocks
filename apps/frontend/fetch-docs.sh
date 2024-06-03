b7251646bda5684202f0546a49a41a19b83d2cbb
Patrick Arminio <patrick.arminio@gmail.com>
2024-06-03 22:52:47 +0100

    Update content

--- apps/frontend/fetch-docs.sh
+++ apps/frontend/fetch-docs.sh
@@ -3,13 +3,14 @@ function clone_docs_from_repo() {
 
     repo=$1
     destination=$working_dir/src/content/$2
+    branch=${3:-main}
 
-    echo "Cloning docs from $repo to $destination"
+    echo "Cloning docs from $repo to $destination on branch $branch"
 
     # move to a temporary directory
     cd $(mktemp -d)
 
-    git clone -n --depth=1 --filter=tree:0 "$repo" > /dev/null 2>&1
+    git clone -n --depth=1 --filter=tree:0 -b $branch "$repo" > /dev/null 2>&1
 
     basename="$(basename "$repo")"
     cd "$basename"
@@ -27,7 +28,7 @@ function clone_docs_from_repo() {
 rm -rf src/content/docs
 
 clone_docs_from_repo "https://github.com/strawberry-graphql/strawberry" "docs"
-clone_docs_from_repo "https://github.com/strawberry-graphql/strawberry-django" "docs/django"
+clone_docs_from_repo "https://github.com/strawberry-graphql/strawberry-django" "docs/django" "feature/new-docs"
 
 # rename all .md files in the docs folder to have a .mdx extension
 
