9dd3f1548271d4852ffb61f2162f7a9875083f85
Patrick Arminio <patrick.arminio@gmail.com>
2024-08-20 10:52:58 +0200

    Remove stellate

--- apps/frontend/src/utils/fetch-polar-sponsors.ts
+++ apps/frontend/src/utils/fetch-polar-sponsors.ts
@@ -3,15 +3,5 @@ import type { Sponsor } from "./types";
 export const fetchPolarSponsors = async () => {
   // temporarily hard coded
 
-  return [
-    {
-      id: "stellate",
-      name: "Stellate",
-      logo: "this is overriden",
-      href: "https://stellate.co/?utm_source=sponsor&utm_medium=strawberry",
-      sponsorship: {
-        monthlyPriceInDollars: 500,
-      },
-    },
-  ] as Sponsor[];
+  return [] as Sponsor[];
 };
