56f81bd2df30a9aedb007ca7b017a37b9aea2403
Patrick Arminio <patrick.arminio@gmail.com>
2025-10-21 23:14:00 +0100

    Fonts and colors

--- apps/strawberry.rocks/app/layout.tsx
+++ apps/strawberry.rocks/app/layout.tsx
@@ -1,14 +1,46 @@
 import "@/app/global.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({
-  subsets: ["latin"],
+const ranade = localFont({
+  src: [
+    {
+      path: "../public/fonts/Ranade-Variable.ttf",
+      weight: "200 900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Ranade-VariableItalic.ttf",
+      weight: "200 900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-ranade",
+});
+
+const satoshi = localFont({
+  src: [
+    {
+      path: "../public/fonts/Satoshi-Variable.ttf",
+      weight: "200 900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Satoshi-VariableItalic.ttf",
+      weight: "200 900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-satoshi",
 });
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${ranade.variable} ${satoshi.variable}`}
+      suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
--- apps/strawberry.rocks/lib/layout.shared.tsx
+++ apps/strawberry.rocks/lib/layout.shared.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "@strawberry-graphql/styleguide/src/components/logo/logo";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
@@ -12,14 +13,7 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
+          <Logo className="h-12 w-auto" />
           Strawberry GraphQL
         </>
       ),
