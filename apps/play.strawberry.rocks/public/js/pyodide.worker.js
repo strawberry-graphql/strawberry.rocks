fdcefc59531d2a7d2c0e0768723cd697a51357ca
Erik <erikwrede@users.noreply.github.com>
2026-01-26 11:59:44 +0100

    fix: upgrade pyodide

--- apps/play.strawberry.rocks/public/js/pyodide.worker.js
+++ apps/play.strawberry.rocks/public/js/pyodide.worker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.25.1/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.29.2/full/pyodide.js");
 
 async function loadPyodideAndPackages(strawberryVersion) {
   self.pyodide = await loadPyodide();
